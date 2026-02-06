6258d143d678be6ba2122c69d2c2cbda388abf40
Marcelo Roberto Jimenez <marcelo.jimenez@gmail.com>
2026-02-05 23:15:01 -0300

    Test

--- scripts/restore_pupnp_files.sh
+++ scripts/restore_pupnp_files.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-git restore ChangeLog configure.ac libupnp.spec docs/Doxyfile docs/changelog_template.txt docs/configure_ac_template.txt
+git restore ChangeLog configure.ac libupnp.spec docs/Doxyfile
 
